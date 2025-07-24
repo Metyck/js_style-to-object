1cf19f78069c37290d89ced5617e5386117aa21c
Matvey Turluk <matychencia.ua@gmail.com>
2025-07-24 16:15:55 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const strings = sourceString
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0);
+
+  for (const string of strings) {
+    const strArr = string.split(':');
+
+    result[strArr[0].trim()] = strArr[1].trim();
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
